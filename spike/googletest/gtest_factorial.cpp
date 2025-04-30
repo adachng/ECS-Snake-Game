31d1453fc69c1a33001d395bcbd3c9f3ac3b3c74
Adam Chiang <11900409+adachng@users.noreply.github.com>
2025-04-30 19:17:02 +0800

    Encapsulate `gtest_factorial` code in namespace

--- spike/googletest/gtest_factorial.cpp
+++ spike/googletest/gtest_factorial.cpp
@@ -1,41 +1,44 @@
 #include <gtest/gtest.h>
 
-int factorial(int n); // Returns the factorial of n
-
-// Tests factorial of 0.
-TEST(FactorialTest, HandlesZeroInput)
+namespace
 {
-    EXPECT_EQ(factorial(0), 1);
-}
+    int factorial(int n); // Returns the factorial of n
 
-// Tests factorial of positive numbers.
-TEST(FactorialTest, HandlesPositiveInput)
-{
-    EXPECT_EQ(factorial(1), 1);
-    EXPECT_EQ(factorial(2), 2);
-    EXPECT_EQ(factorial(3), 6);
-    EXPECT_EQ(factorial(8), 40320);
-    EXPECT_EQ(factorial(9), 362880);
-}
-
-TEST(FactorialTest, HandlesNegativeInput)
-{
-    EXPECT_EQ(factorial(-1), -1);
-    EXPECT_EQ(factorial(-2), 2);
-    EXPECT_EQ(factorial(-3), -6);
-    EXPECT_EQ(factorial(-8), 40320);
-    EXPECT_EQ(factorial(-9), -362880);
-}
-
-int factorial(int n)
-{
-    if (n < 0)
+    // Tests factorial of 0.
+    TEST(FactorialTest, HandlesZeroInput)
     {
-        n *= -1;
-        return (n % 2 == 0) ? factorial(n) : -1 * factorial(n);
+        EXPECT_EQ(factorial(0), 1);
     }
-    else if (n <= 1)
-        return 1;
-    else
-        return n * factorial(n - 1);
-}
+
+    // Tests factorial of positive numbers.
+    TEST(FactorialTest, HandlesPositiveInput)
+    {
+        EXPECT_EQ(factorial(1), 1);
+        EXPECT_EQ(factorial(2), 2);
+        EXPECT_EQ(factorial(3), 6);
+        EXPECT_EQ(factorial(8), 40320);
+        EXPECT_EQ(factorial(9), 362880);
+    }
+
+    TEST(FactorialTest, HandlesNegativeInput)
+    {
+        EXPECT_EQ(factorial(-1), -1);
+        EXPECT_EQ(factorial(-2), 2);
+        EXPECT_EQ(factorial(-3), -6);
+        EXPECT_EQ(factorial(-8), 40320);
+        EXPECT_EQ(factorial(-9), -362880);
+    }
+
+    int factorial(int n)
+    {
+        if (n < 0)
+        {
+            n *= -1;
+            return (n % 2 == 0) ? factorial(n) : -1 * factorial(n);
+        }
+        else if (n <= 1)
+            return 1;
+        else
+            return n * factorial(n - 1);
+    }
+} // namespace
