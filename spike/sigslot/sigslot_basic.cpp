1bae751dcc12a27c7d335abca5805030df0f3a06
Adam Chiang <11900409+adachng@users.noreply.github.com>
2025-05-01 11:59:07 +0800

    Add `spike/sigslot/sigslot_basic.cpp`

---
+++ spike/sigslot/sigslot_basic.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+#include <sigslot/signal.hpp>
+
+void f() { std::cout << "free function" << std::endl; }
+
+struct o
+{
+    void operator()() { std::cout << "function object" << std::endl; }
+};
+
+void fArg(float &x)
+{
+    std::cout << "Adding 2 to x which is " << x << std::endl;
+    x += 2.0f;
+}
+
+struct oArg
+{
+    void operator()(float &x)
+    {
+        std::cout << "Adding 2 to x which is " << x << std::endl;
+        x += 2.0f;
+    }
+};
+
+int main()
+{
+    // Without arguments
+    auto lambda = []()
+    { std::cout << "lambda\n"; };
+
+    sigslot::signal<> sigNoArg;
+
+    sigNoArg.connect(f);
+    sigNoArg.connect(o());
+    sigNoArg.connect(lambda);
+
+    sigNoArg();
+
+    // With arguments
+    float arg = 1.23f;
+    sigslot::signal<float &> sig;
+
+    sig.connect(fArg);
+    sig.connect(oArg());
+
+    std::cout << "x = " << arg << std::endl;
+    sig(arg);
+    std::cout << "x = " << arg << std::endl;
+
+    return 0;
+}
