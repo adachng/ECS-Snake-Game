90f062f500cda5ad5320a64bfb7fd5cea61b5112
Adam Chiang <11900409+adachng@users.noreply.github.com>
2025-05-02 21:16:46 +0800

    Move edge cases to bottom in `translate_2d_test`

--- test/translate_2d_test.cpp
+++ test/translate_2d_test.cpp
@@ -7,41 +7,6 @@
 
 namespace
 {
-    TEST(Translate2DSystemTest, MultipleInit)
-    {
-        sigslot::signal<entt::registry &> gameplaySceneSignal;
-        sigslot::signal<entt::registry &> mainMenuSceneSignal;
-        sigslot::signal<entt::registry &> creditsSceneSignal;
-
-        EXPECT_TRUE(SystemTranslate2D::init(gameplaySceneSignal));
-        EXPECT_TRUE(SystemTranslate2D::init(mainMenuSceneSignal));
-        EXPECT_TRUE(SystemTranslate2D::init(creditsSceneSignal));
-
-        EXPECT_FALSE(SystemTranslate2D::init(gameplaySceneSignal));
-        EXPECT_FALSE(SystemTranslate2D::init(mainMenuSceneSignal));
-        EXPECT_FALSE(SystemTranslate2D::init(creditsSceneSignal));
-
-        EXPECT_FALSE(SystemTranslate2D::init(gameplaySceneSignal));
-        EXPECT_FALSE(SystemTranslate2D::init(mainMenuSceneSignal));
-        EXPECT_FALSE(SystemTranslate2D::init(creditsSceneSignal));
-    }
-
-    TEST(Translate2DSystemTest, DeltaTimeMissing)
-    {
-        entt::registry reg;
-        auto entity = reg.create();
-        reg.emplace<Position>(entity, 0.0f, 0.0f);
-        reg.emplace<Velocity>(entity, 12.0f, -100.0f);
-
-        for (int i = 1; i <= 100; i++)
-            SystemTranslate2D::iterate(reg);
-
-        Position pos = reg.get<Position>(reg.view<Position>().front());
-
-        EXPECT_FLOAT_EQ(pos.x, 0.0f);
-        EXPECT_FLOAT_EQ(pos.y, 0.0f);
-    }
-
     TEST(Translate2DSystemTest, ZeroVelocity)
     {
         entt::registry reg;
@@ -128,5 +93,40 @@ namespace
 
         EXPECT_FLOAT_EQ(pos.x, expectedX);
         EXPECT_FLOAT_EQ(pos.y, expectedY);
+    }
+
+    TEST(Translate2DSystemTest, DeltaTimeMissing)
+    {
+        entt::registry reg;
+        auto entity = reg.create();
+        reg.emplace<Position>(entity, 0.0f, 0.0f);
+        reg.emplace<Velocity>(entity, 12.0f, -100.0f);
+
+        for (int i = 1; i <= 100; i++)
+            SystemTranslate2D::iterate(reg);
+
+        Position pos = reg.get<Position>(reg.view<Position>().front());
+
+        EXPECT_FLOAT_EQ(pos.x, 0.0f);
+        EXPECT_FLOAT_EQ(pos.y, 0.0f);
+    }
+
+    TEST(Translate2DSystemTest, MultipleInit)
+    {
+        sigslot::signal<entt::registry &> gameplaySceneSignal;
+        sigslot::signal<entt::registry &> mainMenuSceneSignal;
+        sigslot::signal<entt::registry &> creditsSceneSignal;
+
+        EXPECT_TRUE(SystemTranslate2D::init(gameplaySceneSignal));
+        EXPECT_TRUE(SystemTranslate2D::init(mainMenuSceneSignal));
+        EXPECT_TRUE(SystemTranslate2D::init(creditsSceneSignal));
+
+        EXPECT_FALSE(SystemTranslate2D::init(gameplaySceneSignal));
+        EXPECT_FALSE(SystemTranslate2D::init(mainMenuSceneSignal));
+        EXPECT_FALSE(SystemTranslate2D::init(creditsSceneSignal));
+
+        EXPECT_FALSE(SystemTranslate2D::init(gameplaySceneSignal));
+        EXPECT_FALSE(SystemTranslate2D::init(mainMenuSceneSignal));
+        EXPECT_FALSE(SystemTranslate2D::init(creditsSceneSignal));
     }
 } // namespace
